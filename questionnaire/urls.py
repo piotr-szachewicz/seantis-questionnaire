f225baf756d16580ece398bf54d90992965951e2
Piotr Szachewicz <piotr.szachewicz@gmail.com>
2014-04-22 17:53:31 +0200

    Reformat URLS.py file.

--- questionnaire/urls.py
+++ questionnaire/urls.py
@@ -4,14 +4,9 @@ from django.conf.urls import *
 from views import *
 
 urlpatterns = patterns('',
-    url(r'^$',
-            questionnaire, name='questionnaire_noargs'),
-    url(r'^csv/(?P<qid>\d+)/$',
-            export_csv, name='export_csv'),
-    url(r'^(?P<runcode>[^/]+)/progress/$', 
-            get_async_progress, name='progress'),
-    url(r'^(?P<runcode>[^/]+)/(?P<qs>[-]{0,1}\d+)/$',
-            questionnaire, name='questionset'),
-    url(r'^(?P<runcode>[^/]+)/$',
-            questionnaire, name='questionnaire'),
+    url(r'^$', questionnaire, name='questionnaire_noargs'),
+    url(r'^csv/(?P<qid>\d+)/$', export_csv, name='export_csv'),
+    url(r'^(?P<runcode>[^/]+)/progress/$',  get_async_progress, name='progress'),
+    url(r'^(?P<runcode>[^/]+)/(?P<qs>[-]{0,1}\d+)/$', questionnaire, name='questionset'),
+    url(r'^(?P<runcode>[^/]+)/$', questionnaire, name='questionnaire'),
 )
